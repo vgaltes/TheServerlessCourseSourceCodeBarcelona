add54fa32cec04afa3b5de80c20870472eefebc8
Vicenç García Altés <vgales@gmail.com>
2019-10-20 22:29:26 +0100

    Lesson21

--- src/functions/notifyOrganiser.js
+++ src/functions/notifyOrganiser.js
@@ -1,7 +1,14 @@
-module.exports.handler = async (event, context) => {
+const Log = require('@dazn/lambda-powertools-logger');
+const middy = require("middy");
+const correlationIds = require('@dazn/lambda-powertools-middleware-correlation-ids');
+
+const handler = async (event, context) => {
     const orderPlaced = JSON.parse(event.Records[0].Sns.Message);
   
-    console.log(`notified organiser [${orderPlaced.getTogetherId}, ${orderPlaced.orderId}, ${orderPlaced.userEmail}]`);
+    Log.info('notified organiser', { getTogetherId: orderPlaced.getTogetherId, orderId: orderPlaced.orderId, userEmail: orderPlaced.userEmail});
   
     return "all done";
 };
+
+module.exports.handler = middy(handler)
+    .use(correlationIds({ sampleDebugLogRate: 0 }));
