253fff7cdc90f6571e4de3d6c60a46d494d89f8c
Vicenç García Altés <vgales@gmail.com>
2019-10-21 22:13:42 +0100

    Lesson25

--- src/functions/getGetTogethers.js
+++ src/functions/getGetTogethers.js
@@ -1,12 +1,25 @@
 const AWS = require("aws-sdk");
 const middy = require("middy");
 const { ssm } = require("middy/middlewares");
+const FunctionShield = require('@puresec/function-shield');
+FunctionShield.configure({
+    policy: {
+        // 'block' mode => active blocking
+        // 'alert' mode => log only
+        // 'allow' mode => allowed, implicitly occurs if key does not exist
+        outbound_connectivity: "alert",
+        read_write_tmp: "block", 
+        create_child_process: "block",
+        read_handler: "block" },
+    token: process.env.functionShieldToken });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const handler = async (evt, context) => {
   const count = 8;
   const tableName = context.tableName;
+
+  http.get('http://vgaltes.com');
 
   const req = {
     TableName: tableName,
